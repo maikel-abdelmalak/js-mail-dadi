2a05b1ec7fd077ef3ad839755eee3ee705936555
maikel abdelmalak <maikel22@hotmail.it>
2020-04-22 12:07:22 +0200

    aggiunta pareggio

--- dadi/main.js
+++ dadi/main.js
@@ -1,12 +1,14 @@
 // creo due numeri random tra 1 e 6
-var rossa = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+var rossa = Math.floor(Math.random() * 6) + 1;
 console.log('la squadra rossa' + rossa);
 
-var verde = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+var verde = Math.floor(Math.random() * 6) + 1;
 console.log('la squadra verde' + verde);
 
 //chiedo all'utente se vuole giocare con la squadra rossa o la squadra
 var scelta_utente = prompt('scegli la squadra rossa o la squadra verde', 'rossa o verde');
+
+
 
 // se l'utente sceglie la squadra con il numero più alto vince altrimenti vince il pc
 if (scelta_utente == 'rossa' && rossa > verde){
@@ -15,6 +17,8 @@ if (scelta_utente == 'rossa' && rossa > verde){
 }else if (scelta_utente == 'verde' && verde > rossa) {
     console.log('hai vinto');
     document.getElementById('si').setAttribute('class', 'visible');
+}else if(rossa == verde){
+    document.getElementById('pari').setAttribute('class', 'visible');
 }else{
     console.log('hai perso');
     document.getElementById('no').setAttribute('class', 'visible');
